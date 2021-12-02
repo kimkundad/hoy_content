49dc9bbebf78c0fc48b7666b9f062ab584447666
kimkundad <kim.kundad@gmail.com>
2021-12-02 18:34:02 +0700

    kim

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const init = async () => {
             
             return new Promise((resolve, reject) => {
             request(url, (err, response, body) => {
-                console.log('--', body)
+                
                 if (!err && response.statusCode === 200) {
             
                     var list = [];
@@ -94,13 +94,10 @@ const init = async () => {
 
     },
     {
-
         method: 'GET',
         path: '/gsb_thai',
         handler: async function (req, h) {
-
             let url = `${URL}`
-            
             return new Promise((resolve, reject) => {
             request(url, (err, response, body) => {
                 console.log('--', response.statusCode)
@@ -121,10 +118,142 @@ const init = async () => {
                 }
             })
         })
-
-            
         }
-
+    },
+    {
+        method: 'GET',
+        path: '/hanoy',
+        handler: async function (req, h) {
+            let url = `${URL}`
+            return new Promise((resolve, reject) => {
+            request(url, (err, response, body) => {
+                console.log('--', response.statusCode)
+                if (!err && response.statusCode === 200) {
+        
+                    var list2 = [];
+                    let $ = cheerio.load(body)
+                    
+                      $('div[class="table-num"]').find('div > div > div > p').each(function (index, element) {
+                        list2.push($(element).text().trim());
+                      });
+                    const user = {
+                        ThreeTop: list2[9],
+                        TwoUnder: list2[11]
+                    }
+                    return resolve(user);
+                    
+                }
+            })
+        })
+        }
+    },
+    {
+        method: 'GET',
+        path: '/hanoySP',
+        handler: async function (req, h) {
+            let url = `${URL}`
+            return new Promise((resolve, reject) => {
+            request(url, (err, response, body) => {
+                console.log('--', response.statusCode)
+                if (!err && response.statusCode === 200) {
+        
+                    var list2 = [];
+                    let $ = cheerio.load(body)
+                    
+                      $('div[class="table-num"]').find('div > div > div > p').each(function (index, element) {
+                        list2.push($(element).text().trim());
+                      });
+                    const user = {
+                        ThreeTop: list2[25],
+                        TwoUnder: list2[27]
+                    }
+                    return resolve(user);
+                    
+                }
+            })
+        })
+        }
+    },
+    {
+        method: 'GET',
+        path: '/lao',
+        handler: async function (req, h) {
+            let url = `${URL}`
+            return new Promise((resolve, reject) => {
+            request(url, (err, response, body) => {
+                console.log('--', response.statusCode)
+                if (!err && response.statusCode === 200) {
+        
+                    var list2 = [];
+                    let $ = cheerio.load(body)
+                    
+                      $('div[class="table-num"]').find('div > div > div > p').each(function (index, element) {
+                        list2.push($(element).text().trim());
+                      });
+                    const user = {
+                        ThreeTop: list2[13],
+                        TwoUnder: list2[15]
+                    }
+                    return resolve(user);
+                    
+                }
+            })
+        })
+        }
+    },
+    {
+        method: 'GET',
+        path: '/stock_nikkeiday',
+        handler: async function (req, h) {
+            let url = `${URL}`
+            return new Promise((resolve, reject) => {
+            request(url, (err, response, body) => {
+                console.log('--', response.statusCode)
+                if (!err && response.statusCode === 200) {
+        
+                    var list2 = [];
+                    let $ = cheerio.load(body)
+                    
+                      $('div[class="table-num"]').find('div > div > div > p').each(function (index, element) {
+                        list2.push($(element).text().trim());
+                      });
+                    const user = {
+                        ThreeTop: list2[36],
+                        TwoUnder: list2[37]
+                    }
+                    return resolve(user);
+                    
+                }
+            })
+        })
+        }
+    },
+    {
+        method: 'GET',
+        path: '/stock_nikkeinoon',
+        handler: async function (req, h) {
+            let url = `${URL}`
+            return new Promise((resolve, reject) => {
+            request(url, (err, response, body) => {
+                console.log('--', response.statusCode)
+                if (!err && response.statusCode === 200) {
+        
+                    var list2 = [];
+                    let $ = cheerio.load(body)
+                    
+                      $('div[class="table-num"]').find('div > div > div > p').each(function (index, element) {
+                        list2.push($(element).text().trim());
+                      });
+                    const user = {
+                        ThreeTop: list2[38],
+                        TwoUnder: list2[39]
+                    }
+                    return resolve(user);
+                    
+                }
+            })
+        })
+        }
     },
 ]);
 
